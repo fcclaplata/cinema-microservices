527b3fb2b6a029ab768246150cf9b7b28668ba9a
Agustin <agustinmedina11@gmail.com>
2017-06-25 20:16:58 -0300

    app dockerized

--- movies-service/project/db.py
+++ movies-service/project/db.py
@@ -3,13 +3,18 @@ from os import environ
 
 # f"mongodb://{environ.get('DBUSER')}:{environ.get('DBPWD')}@{environ.get('DBHOST')}:{environ.get('DBPORT')}{environ.get('DBPATH')}")
 
+DBUSER = environ.get('DBUSER')
+DBPWD = environ.get('DBPWD')
+DBHOST = environ.get('DBHOST')
+DBPORT = environ.get('DBPORT')
+DBNAME = environ.get('DBNAME')
 
 class Database():
 
     def __init__(self):
         client = MongoClient(
-            f"mongodb://mongo:mongo@localhost:27017")
-        db = client['admin']
+            f"mongodb://{DBUSER}:{DBPWD}@{DBHOST}:{DBPORT}")
+        db = client[DBNAME]
         self.collection = db.movies
 
     def movies(self):

3920740438f885dc7fa7d265e0b2448ff1bc5815
Nacho Althabe <nacho.althabe@gmail.com>
2017-07-15 20:39:14 -0300

    Muetra las peliculas, pero haria falta acomodar la clase DB para usar menos conf

--- movies-service/src/project/db.py
+++ movies-service/src/project/db.py
@@ -2,13 +2,14 @@ from pymongo import MongoClient
 from os import environ
 
 DBURL = environ.get('DBURL')
+DBNAME = environ.get('DBNAME')
 
 
 class Database():
 
     def __init__(self):
         client = MongoClient(DBURL)
-        db = client
+        db = client[DBNAME]
         self.collection = db.movies
 
     def movies(self):

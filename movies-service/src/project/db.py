53b58cf023038dcc4f88ee75d355dd053ea84ac0
Federico Marcos <marcosfede@gmail.com>
2017-07-15 17:15:55 -0300

    change structure

--- movies-service/src/project/db.py
+++ movies-service/src/project/db.py
@@ -1,19 +1,17 @@
 from pymongo import MongoClient
 from os import environ
 
-# f"mongodb://{environ.get('DBUSER')}:{environ.get('DBPWD')}@{environ.get('DBHOST')}:{environ.get('DBPORT')}{environ.get('DBPATH')}")
-
 DBUSER = environ.get('DBUSER')
 DBPWD = environ.get('DBPWD')
 DBHOST = environ.get('DBHOST')
 DBPORT = environ.get('DBPORT')
 DBNAME = environ.get('DBNAME')
 
+
 class Database():
 
     def __init__(self):
-        client = MongoClient(
-            f"mongodb://{DBUSER}:{DBPWD}@{DBHOST}:{DBPORT}")
+        client = MongoClient(f"mongodb://{DBUSER}:{DBPWD}@{DBHOST}:{DBPORT}")
         db = client[DBNAME]
         self.collection = db.movies
 
